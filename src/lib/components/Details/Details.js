4330282caa2b40e933e79723bbf9dcb155f5181e
Jerryh2-0 <supermighty0.cubeway2.mcpe@gmail.com>
2020-12-26 14:37:52 +0100

    changed color of text

--- src/lib/components/Details/Details.js
+++ src/lib/components/Details/Details.js
@@ -14,9 +14,9 @@ class Details extends Component {
                     this.props.details === 'gps' ?
                         <iframe name="marinetraffic" id="marinetraffic" scrolling="no" src="https://www.marinetraffic.com/en/ais/embed/zoom:2/centery:36/centerx:23/maptype:0/shownames:false/mmsi:248758000/shipid:0/fleet:/fleet_id:/vtypes:/showmenu:/remember:false" width="90%" height="80%" frameborder="1">Browser does not support embedded objects.<br/>Visit directly <a href="http://www.marinetraffic.com/">www.marinetraffic.com</a></iframe>
                     : this.props.details === 'map' ?
-                        <div id='map'>Carte pas encore disponible</div>
+                        <div id='map' style={{ color: 'white' }}>Carte pas encore disponible</div>
                     :
-                        <div id='book'>Carnet de bord pas encore disponible</div>
+                        <div id='book' style={{ color: 'white' }}>Carnet de bord pas encore disponible</div>
                 }
             </div>
         )
