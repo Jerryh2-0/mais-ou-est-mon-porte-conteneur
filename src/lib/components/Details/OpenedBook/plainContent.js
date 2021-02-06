c721a987aaf5c2700df27569676aa46e84d14917
Jerryh2-0 <supermighty0.cubeway2.mcpe@gmail.com>
2021-02-06 16:20:37 +0100

    added the biggest part of the log book from december (until 26 december)

--- src/lib/components/Details/OpenedBook/plainContent.js
+++ src/lib/components/Details/OpenedBook/plainContent.js
@@ -7,13 +7,36 @@ let plainContent = [
     ],
     [
         ['h2', 'Jeudi 10 décembre 2020, 22h07'],
-        ["p", "Me voici pour la première fois à bord d'un porte-conteneur, le Jean Mermoz. Mon rêve de devenir marin devient enfin réalité. Quelle chance qu'un matelot soit tombé malade et que CMA CGM accepte ma candidature."]
+        ["p", "J'embarque pour la première fois à bord d'un navire, un porte-conteneur, le Jean Mermoz, ici, au port de Anchorage en Egypte. Notre navire a pour but de relier l'Europe à l'Asie. Mon rêve de devenir marin devient enfin réalité. Quelle chance qu'un matelot soit en congé maladie après avoir attrapé la Covid-19 et que CMA CGM ait accepté ma candidature. J'ai hâte de vivre cette belle aventure et de visiter d'autres pays que la France pendant cette période de pandémie."],
+
+        ['h2', 'Vendredi 11 décembre 2020, 4h41'],
+        ['p', 'Nous arrivons au port du canal de Suez où nous allons faire une halte.'],
+
+        ['h2', 'Vendredi 11 décembre 2020, 11h37'],
+        ['p', 'Nous sommes actuellement au large de l\'Egypte et nous nous voguons à la vitesse de 9.1 noeuds.']
     ],
     [
+        ['h2', 'Samedi 12 décembre 2020, 11h50'],
+        ['p', 'Nous voici en mer Rouge, nous nous dirigeons toujours vers le Sud à une vitesse de 20.1 noeuds pour déboucher sur l\'océan Indien. Un doux vent de force 1 souffle actuellement dans la zone.'],
 
+        ['h2', 'Dimanche 13 décembre 2020, 17h34'],
+        ['p', 'Nous sommes toujours en mer Rouge et nous naviguons à 19.1 noeuds. Un vent de force 4 souffle aujourd\'hui.'],
+
+        ['h2', 'Samedi 19 décembre 2020, 3h18'],
+        ['p', 'Désolé cher carnet de bord, je n\'ai pas pu te remplir ces derniers jours à cause de mon mal de mer. Nous sommes maintenant au sud du Sri Lanka, dans l\'océan Indien. J\'espère que l\'agréable, humide et chaud vent de force 2 me permettra de me remettre de ma maladie.']
     ],
     [
+        ['h2', 'Lundi 21 décembre 2020, 20h29'],
+        ['p', 'Nous voilà bientôt arrivé en Malaisie. Un vent de force 4 peut-être ressenti.'],
 
+        ['h2', 'Mardi 22 décembre 2020, 19h56'],
+        ['p', 'Nous sommes actuellement dans le détroit de Malacca en but d\'aller au port de Klang en Malaisie. Un vent de force 3 souffle dans le détroit actuellement.'],
+
+        ['h2', 'Mercredi 23 décembre 2020, 5h12'],
+        ['p', 'Nous voici amarrés au port Klang, nous allons y rester jusqu\'au lendemain de Noël pour nous reposer un peu.'],
+
+        ['h2', 'Samedi 26 décembre 2020, 13h54'],
+        ['p', 'Nous revoilà partis en direction de la Chine à une vitesse de 13.4 noeuds avec un vent de force 4.']
     ],
     [
 
