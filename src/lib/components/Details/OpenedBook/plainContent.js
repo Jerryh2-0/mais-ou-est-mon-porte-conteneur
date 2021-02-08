421b1bbce0b7c4e563695783c05cf1beebf29a2c
Kévin BUI <supermighty0.cubeway2.mcpe@gmail.com>
2021-02-08 13:36:57 +0100

    added coordinatres to the previous logs

--- src/lib/components/Details/OpenedBook/plainContent.js
+++ src/lib/components/Details/OpenedBook/plainContent.js
@@ -13,33 +13,31 @@ let plainContent = [
         ['p', 'Nous arrivons au port du canal de Suez où nous allons faire une halte.'],
 
         ['h2', 'Vendredi 11 décembre 2020, 11h37'],
-        ['p', 'Nous sommes actuellement au large de l\'Egypte et nous nous voguons à la vitesse de 9.1 noeuds.']
+        ['p', 'Nous sommes actuellement au large de l\'Egypte, à 30.1217° Nord et 32.5701° Est et nous nous voguons à la vitesse de 9.1 noeuds.']
     ],
     [
         ['h2', 'Samedi 12 décembre 2020, 11h50'],
-        ['p', 'Nous voici en mer Rouge, nous nous dirigeons toujours vers le Sud à une vitesse de 20.1 noeuds pour déboucher sur l\'océan Indien. Un doux vent de force 1 souffle actuellement dans la zone.'],
+        ['p', 'Nous voici en mer Rouge aux coordonnées 23° 59\' 40.2" N, 36° 13\' 10.308" E, nous nous dirigeons toujours vers le Sud à une vitesse de 20.1 noeuds pour déboucher sur l\'océan Indien. Un doux vent de force 1 souffle actuellement dans la zone.'],
 
         ['h2', 'Dimanche 13 décembre 2020, 17h34'],
-        ['p', 'Nous sommes toujours en mer Rouge et nous naviguons à 19.1 noeuds. Un vent de force 4 souffle aujourd\'hui.'],
+        ['p', 'Nous sommes maintenant en 15° 40\' 55.272" N, 41° 17\' 58.848" E, toujours en mer Rouge et nous naviguons à 19.1 noeuds. Un vent de force 4 souffle aujourd\'hui.'],
 
         ['h2', 'Samedi 19 décembre 2020, 3h18'],
-        ['p', 'Désolé cher carnet de bord, je n\'ai pas pu te remplir ces derniers jours à cause de mon mal de mer. Nous sommes maintenant au sud du Sri Lanka, dans l\'océan Indien. J\'espère que l\'agréable, humide et chaud vent de force 2 me permettra de me remettre de ma maladie.']
+        ['p', 'Désolé cher carnet de bord, je n\'ai pas pu te remplir ces derniers jours à cause de mon mal de mer. Nous sommes maintenant en 5° 43\' 6.96" N, 80° 37\' 14.988" E, au sud du Sri Lanka, dans l\'océan Indien. J\'espère que l\'agréable, humide et chaud vent de force 2 me permettra de me remettre de ma maladie.']
     ],
     [
         ['h2', 'Lundi 21 décembre 2020, 20h29'],
-        ['p', 'Nous voilà bientôt arrivé en Malaisie. Un vent de force 4 peut-être ressenti.'],
+        ['p', 'Nous voilà en 6° 6\' 47.642" N, 95° 18\' 17.1" E, nous sommes bientôt arrivé en Malaisie. Un vent de force 4 peut-être ressenti.'],
 
         ['h2', 'Mardi 22 décembre 2020, 19h56'],
-        ['p', 'Nous sommes actuellement dans le détroit de Malacca en but d\'aller au port de Klang en Malaisie. Un vent de force 3 souffle dans le détroit actuellement.'],
+        ['p', 'Nous sommes actuellement en 3° 35\' 17.268" N, 100° 2\' 19.572" E, dans le détroit de Malacca en but d\'aller au port de Klang en Malaisie. Un vent de force 3 souffle dans le détroit actuellement.'],
 
         ['h2', 'Mercredi 23 décembre 2020, 5h12'],
-        ['p', 'Nous voici amarrés au port Klang, nous allons y rester jusqu\'au lendemain de Noël pour nous reposer un peu.'],
-
-        ['h2', 'Samedi 26 décembre 2020, 13h54'],
-        ['p', 'Nous revoilà partis en direction de la Chine à une vitesse de 13.4 noeuds avec un vent de force 4.']
+        ['p', 'Nous voici amarrés au port Klang, nous allons y rester jusqu\'au lendemain de Noël pour nous reposer un peu.']
     ],
     [
-
+        ['h2', 'Samedi 26 décembre 2020, 13h54'],
+        ['p', 'Nous revoilà partis en direction de la Chine à une vitesse de 13.4 noeuds avec un vent de force 4. Nous sommes actuellement en 1° 6\' 41.699" N, 103° 32\' 14.64" E']
     ],
     [
 
