84bc2d1d13a4db93c07a1323552d2e1c2f4bf3eb
Jerryh <supermighty0.cubeway2.mcpe@gmail.com>
2021-03-20 16:27:45 +0100

    added the map with an iframe

--- src/lib/components/Details/Details.js
+++ src/lib/components/Details/Details.js
@@ -18,7 +18,7 @@ class Details extends Component {
                     this.props.details === 'gps' ?
                         <iframe name="marinetraffic" id="marinetraffic" scrolling="no" src="https://www.marinetraffic.com/en/ais/embed/zoom:2/centery:36/centerx:23/maptype:0/shownames:false/mmsi:248758000/shipid:0/fleet:/fleet_id:/vtypes:/showmenu:/remember:false" width="90%" height="80%" frameborder="1">Browser does not support embedded objects.<br/>Visit directly <a href="http://www.marinetraffic.com/">www.marinetraffic.com</a></iframe>
                     : this.props.details === 'map' ?
-                        <div id='map' style={{ color: 'white' }}>Carte pas encore disponible</div>
+                        <div id='map' style={{ color: 'white' }}><iframe src="https://storymaps.arcgis.com/stories/3d22050f524240da814e89b725413229" width="100%" height="100%" frameborder="0" allowfullscreen allow="geolocation"></iframe></div>
                     :
                         <OpenedBook />
                 }
